ae1c95cc880b195260bbd982fab425bbdaedb192
pfdm <1985633909@qq.com>
2022-11-30 19:22:26 +0800

    master commit2 v1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,8 +11,8 @@ public class GitTest {
         System.out.println("hello git...");
         System.out.println("hello git2222...");
         System.out.println("hello git3333...");
-        System.out.println("master test git4444...");
         System.out.println("hello git4444...");
+        System.out.println("master test git4444...");
         System.out.println("hot-fix test git4444...");
 
     }

c8f9758f023bed258a9a303f5c55ac5a41936eaa
pfdm <1985633909@qq.com>
2022-11-30 14:01:42 +0800

    fist commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author: 19856
+ * @date: 2022/11/30-13:55
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git...");
+
+    }
+}

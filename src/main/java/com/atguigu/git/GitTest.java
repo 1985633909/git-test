db12eef5fd1e71216d7d67bccd674d94ed01dca5
1985633909 <104576569+1985633909@users.noreply.github.com>
2022-11-30 19:59:06 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("master test git4444...");
         System.out.println("hot-fix test git4444...");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }

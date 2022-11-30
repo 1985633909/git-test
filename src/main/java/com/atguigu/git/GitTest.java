7db40110b3e6d039879682da201a6f09a0ec3ff5
pfdm <1985633909@qq.com>
2022-11-30 20:39:19 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hot-fix test git4444...");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee ma test");
 
     }
 }
